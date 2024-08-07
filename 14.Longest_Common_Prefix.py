7fe274f6839f29229d1b8e18b52e2f878dc2ea16
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2024-08-07 16:06:03 +0000

    container config done

--- 14.Longest_Common_Prefix.py
+++ 14.Longest_Common_Prefix.py
@@ -15,6 +15,14 @@
 
 # Solution
 
-
+# class Solution:
+#     def longestCommonPrefix(self, strs: List[str])
+#         res = ""
+#         for i in range(len(strs[0])):
+#             for s in strs:
+#                 if i == len(s) or s[i] != strs[0][i]:
+#                     return res
+#             res += strs[0][i]
+#         return res            
 
 
