125cea9413f6c54fc08aa2629a3a7941da23d44e
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2024-08-15 18:09:14 +0000

    container config done

--- 27. Remove_Element.py
+++ 27. Remove_Element.py
@@ -9,3 +9,15 @@
 
 
 
+# ---------Solution--------->
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         i = 0
+#         j = 0
+
+#         while i < len(nums):
+#             if nums[i] != val:
+#                 nums[j] = nums[i]
+#                 j += 1
+#             i += 1    
+#         return j
