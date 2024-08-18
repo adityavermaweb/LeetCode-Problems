369e4e1ee3ca22aba98f7e6c83d6dc56b47d4eb9
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2024-08-18 15:37:16 +0000

    container config done

--- 35.Search_Insert_Position.py
+++ 35.Search_Insert_Position.py
@@ -16,19 +16,20 @@
 # Output: 4
 
 # -------------Solution------------->
-# class Solution:
-#     def searchInsert(self, nums: List[int], target: int) -> int:
-#         l, r = 0, len(nums) -1
 
-#         while l <= r:
-#             mid = (l + r) // 2
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) -1
 
-#             if target == nums[mid]:
-#                 return mid
+        while l <= r:
+            mid = (l + r) // 2
 
-#             if target > nums[mid]:
-#                 l = mid + 1
-#             else:
-#                 r = mid - 1
+            if target == nums[mid]:
+                return mid
 
-#         return l
+            if target > nums[mid]:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return l
