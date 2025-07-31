92248a4e8d28726142c99bd24635734d5e1a41c1
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-07-31 08:01:44 +0000

    container config done

---
+++ Tuf-Problems/Array/1.Largest-Element.java
@@ -0,0 +1,13 @@
+// Array Easy Questions Basic-------------
+
+class Solution {
+    public int largestElement(int[] nums){
+        int max = nums[0];
+        for(int i=0; i<muns.length;i++){
+            if(max<nums[i]){
+                max=nums[i];
+            }
+        }
+        return max;
+    }
+}
